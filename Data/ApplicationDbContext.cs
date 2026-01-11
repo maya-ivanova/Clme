3530a5718ed48ecb2a8d6573d661e9f97ff28a80
maya-ivanova <ivanova.r.maya@gmail.com>
2026-01-11 17:01:38 +0200

    Initial no secrets commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Clme.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Coolme.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
